bc360c5bd3f1119b0b710d967b72a0b7f36332f2
Your Name <ifuenteromer@gmail.com>
2023-05-23 12:23:37 +0200

    hago un listado de ciudades para mapear

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,24 @@
 const ListGroup = () => {
+	const items = [
+		"Abuja",
+		"Port Harcourt",
+		"London",
+		"Paris",
+		"Madrid",
+		"Ibiza",
+		"Dakar",
+		"San Juan",
+	];
+
 	return (
 		<>
 			<h1>List</h1>
 			<ul className="list-group">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-				<li className="list-group-item">A fourth item</li>
-				<li className="list-group-item">And a fifth one</li>
+				{items.map((item) => (
+					<li key={item} className="list-group-item">
+						{item}
+					</li>
+				))}
 			</ul>
 		</>
 	);
