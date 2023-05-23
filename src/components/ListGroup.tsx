497cc51f9de3ceb84aaadc82c1cc281505107511
Your Name <ifuenteromer@gmail.com>
2023-05-23 12:31:48 +0200

    ternarios

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -10,9 +10,12 @@ const ListGroup = () => {
 		"San Juan",
 	];
 
+	const message = items.length === 0 ? <p>No item found</p> : null;
+
 	return (
 		<>
 			<h1>List</h1>
+			{message}
 			<ul className="list-group">
 				{items.map((item) => (
 					<li key={item} className="list-group-item">
