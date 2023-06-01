0262877f38fe61316c609c510f65a61cfb289c71
Irene Fuente <ifuenteromero@gmail.com>
2023-06-01 18:10:42 +0200

    Build a form layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import Button from "./components/Button";
 import ExpandableText from "./components/ExpandableText";
-import Like from "./components/Like";
+import Form from "./components/Form";
 
 const App = () => {
-	const handleClick = () => console.log("click");
 	return (
 		<div>
-			<ExpandableText>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Voluptates necessitatibus ut aperiam quidem nemo quam, enim,
-				corrupti soluta dolores officia explicabo at omnis reprehenderit
-				quisquam earum fugit atque voluptate, eius obcaecati autem
-				pariatur sequi? Qui, fugit. At optio quam quibusdam hic aperiam,
-				alias beatae? Maiores debitis vel recusandae aut dolores, beatae
-				fuga, iste necessitatibus officia accusantium dignissimos
-				voluptate repellat, itaque at nesciunt aliquid ullam quam
-				doloremque ex eos dolorem! Excepturi doloribus accusantium
-				tempore aliquam ratione asperiores? Deserunt amet recusandae
-				corporis natus quo rerum mollitia temporibus non ex. Eveniet
-				sint ad, commodi rem sunt consequuntur voluptate ducimus rerum
-				ipsa! Vitae, totam.
-			</ExpandableText>
+			<Form />
 		</div>
 	);
 };
