382fc2e319e7070e8f48c8eedcb8743c9d894e50
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-12-09 21:32:00 +0100

    11 sharing state between Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import ToDoList from "./components/ToDoList";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 const App = () => {
+	const INITIAL_CART_ITEMS = ["Product 1", "Product 2"];
+	const [cartItems, setCartItems] = useState(INITIAL_CART_ITEMS);
+
+	const handleClear = () => {
+		setCartItems([]);
+	};
+
 	return (
 		<div>
-			<ToDoList />
+			<NavBar cartItemsCount={cartItems.length} />
+			<Cart cartItems={cartItems} onClear={handleClear} />
 		</div>
 	);
 };
