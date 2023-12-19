22e4f3511e80db996dd8984361322e696d866dbb
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-12-19 17:17:27 +0100

    3 Effect dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import EffectHook from "./components/Connecting to the backend/2EffectHook";
+import { ChangeEvent, useState } from "react";
+import ProductList from "./components/Connecting to the backend/ProductList";
 
 const App = () => {
+	const [category, setCategory] = useState("");
+
+	const handleChange = (e: ChangeEvent<HTMLSelectElement>) =>
+		setCategory(e.target.value);
+
 	return (
 		<div>
-			<EffectHook />
+			<select className="form-select" onChange={handleChange}>
+				<option value=""></option>
+				<option value="Clothing">Clothing</option>
+				<option value="Household">Household</option>
+			</select>
+			<ProductList category={category} />
 		</div>
 	);
 };
